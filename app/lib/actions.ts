230e5d59298fdcdeb7780da00a4e088648658733
Wesley Wardlaw <wesley.ryan.wardlaw@gmail.com>
2025-02-12 15:13:16 -0600

    update actions to use generic submitData

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,43 +1,59 @@
-'use server'
+"use server";
 
 import mongoose from "mongoose";
+import { connectToDatabase } from "./mongodb";
+import { ZodSchema } from "zod"; // Import ZodSchema from Zod
 import Riddle from "./models/Riddle";
-import { connectToDatabase } from "./mongodb"
 import { RiddleSchema } from "./schemas/riddle";
+import Saying from "./models/Saying";
+import { SayingSchema } from "./schemas/saying";
 
+type SubmitDataResult = {
+  errors?: Partial<Record<string, string[]>>;
+  success?: boolean;
+};
 
-export async function submitRiddle(formData:FormData){
-    try{
+async function submitData<T>(
+  data: T,
+  Model: mongoose.Model<any>,
+  Schema: ZodSchema<T>
+): Promise<SubmitDataResult> {
+  try {
     await connectToDatabase();
 
-    const rawFormData = {
-        clue: formData.get('clue'),
-        word: formData.get('word'),
+    const validatedFields = Schema.safeParse(data);
+
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors as Partial<Record<string, string[]>>,
+      };
     }
 
-    const validatedFields = RiddleSchema.safeParse(rawFormData);
-
-    if(!validatedFields.success){
-        return { errors: validatedFields.error.flatten().fieldErrors };
-    }
-
-    const { clue, word } = validatedFields.data;
-
-    const newRiddle = new Riddle({clue,word})
-    await newRiddle.save();
+    const newData = new Model(validatedFields.data);
+    await newData.save();
 
     return { success: true };
-} catch (error) {
+  } catch (error) {
+    const errorResponse: SubmitDataResult = { errors: {} };
+
     if (error instanceof mongoose.Error.ValidationError) {
-        return { errors: { general: ["Database validation error"] } };
-      }
-  
-      if (error instanceof mongoose.Error) {
-        return { errors: { general: ["Database error"] } };
-      }
-  
-      // Handle generic server errors
-      return { errors: { general: ["Something went wrong. Please try again later."] } };
+      errorResponse.errors = { root: ["Database validation error"] };
+    } else if (error instanceof mongoose.Error) {
+      errorResponse.errors = { root: ["Database error"] };
+    } else {
+      errorResponse.errors = {
+        root: ["Something went wrong. Please try again later."],
+      };
+    }
+
+    return errorResponse;
+  }
 }
 
+export async function submitRiddle(data: { clue: string; word: string }) {
+    return submitData(data, Riddle, RiddleSchema);
+}
+
+export async function submitSaying(data: { saying: string }) {
+  return submitData(data, Saying, SayingSchema);
 }
