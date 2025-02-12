a63968a3f59313da90e22a3e941cad26f0db25ad
Wesley Wardlaw <wesley.ryan.wardlaw@gmail.com>
2025-02-12 15:10:28 -0600

    add SayingForm to Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import RiddleForm from "./components/RiddleForm";
+import SayingForm from "./components/SayingForm";
 
 
 export default function Home() {
   return (
+    <>
    <RiddleForm />
+   <SayingForm />
+    </>
   );
 }
