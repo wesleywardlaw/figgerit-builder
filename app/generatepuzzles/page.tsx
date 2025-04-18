b4baea209c2c5c9e0c1c5315f6a7c6f7525bde5b
Wesley Wardlaw <wesley.ryan.wardlaw@gmail.com>
2025-04-18 12:24:12 -0500

    remove gray background

--- app/generatepuzzles/page.tsx
+++ app/generatepuzzles/page.tsx
@@ -4,7 +4,7 @@ import GeneratePuzzles from "../components/GeneratePuzzles";
 
 export default function GeneratePuzzlesPage() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
+    <div className="min-h-screen py-8">
       <div className="container mx-auto px-4">
         <GeneratePuzzles />
       </div>
