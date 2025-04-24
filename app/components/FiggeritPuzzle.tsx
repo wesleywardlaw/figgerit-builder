eda2cf54c0bc4cfe942d8d1f27c6c87660d6a4ac
Wesley Wardlaw <wesley.ryan.wardlaw@gmail.com>
2025-04-24 15:56:19 -0500

    update font size

--- app/components/FiggeritPuzzle.tsx
+++ app/components/FiggeritPuzzle.tsx
@@ -21,7 +21,7 @@ const FiggeritPuzzle: React.FC<FiggeritPuzzleProps> = ({ data, saying }) => {
       <div className="grid grid-cols-2 gap-2">
         {data.map((item, idx) => (
           <React.Fragment key={item.riddle._id}>
-            <div className="text-left pl-1 font-medium text-xs flex items-center">
+            <div className="text-left pl-1 font-medium text-[10px] flex items-center">
               {idx + 1}. {item.riddle.clue}
             </div>
 
@@ -40,7 +40,7 @@ const FiggeritPuzzle: React.FC<FiggeritPuzzleProps> = ({ data, saying }) => {
                               key={i}
                               className="flex flex-col items-center w-1"
                             >
-                              <div className="h-4 text-xs w-full text-center">
+                              <div className="h-4 text-[10px] w-full text-center">
                                 {char}
                               </div>
                             </div>
@@ -56,7 +56,7 @@ const FiggeritPuzzle: React.FC<FiggeritPuzzleProps> = ({ data, saying }) => {
                               className="flex flex-col items-center w-3"
                             >
                               <div className="h-4 border-b-2 border-gray-800 text-sm w-full text-center" />
-                              <div className="text-xs text-gray-600">
+                              <div className="text-[10px] text-gray-600">
                                 {number}
                               </div>
                             </div>
@@ -95,7 +95,7 @@ const FiggeritPuzzle: React.FC<FiggeritPuzzleProps> = ({ data, saying }) => {
                 return (
                   <div key={i} className="flex flex-col items-center w-3">
                     <div className="h-4 border-b-2 border-gray-800 text-sm w-full text-center" />
-                    <div className="text-xs text-gray-600">{pos}</div>
+                    <div className="text-[10px] text-gray-600">{pos}</div>
                   </div>
                 );
               })}
